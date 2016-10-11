c8bfd1f7c010b1396ffe37851f6cf1fa855455ec
yanagiragi <qaz90473@gmail.com>
2016-10-12 02:15:46 +0800

    Update buildAssetbundle.bat

--- buildAssetbundle.bat
+++ buildAssetbundle.bat
@@ -4,3 +4,6 @@ SET BUILDPATH="C:\temp\build.exe"
 SET UNITYUSER="yanagiragi"
 
 %UNITYPATH% -batchmode -quit -projectPath %CREATEPROJECTPATH% -executeMethod CreateAssetBundles.CreateAssetBundlesMain -user %UNITYUSER% -buildWindows64Player %BUILDPATH%
+
+rem if you can't understand the code above, you may need to take a look at 
+rem https://docs.unity3d.com/Manual/CommandLineArguments.html
